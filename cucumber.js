5d55b10597dd05a753e2a5201512852083ee2c71
Sahin Bolukbasi <sahin.bolukbasi+prgsft@pargesoft.com>
2026-07-15 14:33:11 +0300

    chore: add npm dependencies and cucumber config

---
+++ cucumber.js
@@ -0,0 +1,42 @@
+const common = {
+  require: [
+    'support/world.js',
+    'support/hooks.js',
+    'step_definitions/**/*.js',
+  ],
+  format: [
+    'progress-bar',
+    'allure-cucumberjs/reporter',
+  ],
+  formatOptions: {
+    resultsDir: './allure-results',
+  },
+  publishQuiet: true,
+};
+
+module.exports = {
+  default: {
+    ...common,
+    paths: ['features/**/*.feature'],
+  },
+  smoke: {
+    ...common,
+    paths: ['features/**/*.feature'],
+    tags: '@smoke',
+  },
+  regression: {
+    ...common,
+    paths: ['features/**/*.feature'],
+    tags: '@regression',
+  },
+  negative: {
+    ...common,
+    paths: ['features/**/*.feature'],
+    tags: '@negative',
+  },
+  parallel: {
+    ...common,
+    paths: ['features/**/*.feature'],
+    parallel: 2,
+  },
+};
